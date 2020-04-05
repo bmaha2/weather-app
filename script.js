c832a8f5ad20c24a3c1669aefb562aef53e2db64
bmaha2 <maharjanbipin@gmail.com>
2020-04-04 20:59:30 -0700

    initial layout

---
+++ script.js
@@ -0,0 +1,36 @@
+
+/*
+submit request for city
+
+create queryURL the user provides
+add it history
+
+when data returned
+extract:
+city name
+date
+an icon for weather condition
+the temperature
+humidity
+wind-speed
+UV index
+within UV
+three conditoins
+add css class 
+favorable
+moderate
+severe
+request for 5day forecast
+append the data to the page
+
+user clicks the history 
+query with that city
+
+api.openweathermap.org/data/2.5/weather?q=London
+api key:
+ ccc5796d60184c50f2674c57d349cd70
+
+
+
+*/
+
